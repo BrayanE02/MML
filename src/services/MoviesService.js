2813fdc2a35b3c75f709cd8a4c2333342def175e
Oneshot369 <joshcpeck4@gmail.com>
2024-03-19 18:47:37 -0700

    fixed api calls

--- src/services/MoviesService.js
+++ src/services/MoviesService.js
@@ -4,6 +4,7 @@ import MovieDataService from "./MovieDataService";
 
 // gets one movie by ID
 export async function getOneMovieById(ID){
+    //header of request
     const options = {
         method: 'GET',
         headers: {
@@ -11,9 +12,11 @@ export async function getOneMovieById(ID){
           Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZGE0OGI3OGNjNjg2Mjc4M2IwOTljZjM4NTZhNWVmYyIsInN1YiI6IjY1OWMzMTQyMWQxYmY0NGJjYjlhZjFiOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lf4D-yEESLD5dKU5H55ghAsClMPo1gsr6QpKTB0EpXI'
         }
       };
-      
+    //url of request
+    var url = `movie/${ID}?language=en-US`;
+    //try the request
     try {
-        const response = await MovieDataService.get(`https://api.themoviedb.org/3/movie/${ID}?language=en-US`, options);
+        const response = await MovieDataService.get(url, options);
         const movieData = response.data;
         console.log("Responce:", movieData)
         return movieData;
@@ -23,4 +26,51 @@ export async function getOneMovieById(ID){
     }
 }
 
+// gets the top movies any genera
+export async function getTopMovies(){
+    //header of request
+    const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZGE0OGI3OGNjNjg2Mjc4M2IwOTljZjM4NTZhNWVmYyIsInN1YiI6IjY1OWMzMTQyMWQxYmY0NGJjYjlhZjFiOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lf4D-yEESLD5dKU5H55ghAsClMPo1gsr6QpKTB0EpXI'
+        }
+      };
+    //URL of request
+    var url = "discover/movie?page=1&language=en-US&include_video=false&sort_by=popularity.desc&primary_release_year=2024"
+    
+    //try the request
+    try {
+        const response = await MovieDataService.get(url, options);
+        const movieData = response.data;
+        console.log("Responce:", movieData)
+        return movieData;
+    } 
+    catch (err) {
+        console.error(err);
+    }
+}
 
+// gets top comedy movies
+export async function getTopComedyMovies(){
+    //header of request
+    const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZGE0OGI3OGNjNjg2Mjc4M2IwOTljZjM4NTZhNWVmYyIsInN1YiI6IjY1OWMzMTQyMWQxYmY0NGJjYjlhZjFiOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lf4D-yEESLD5dKU5H55ghAsClMPo1gsr6QpKTB0EpXI'
+        }
+      };
+    //URL of request
+    var url = "discover/movie?page=1&language=en-US&include_video=false&sort_by=popularity.desc&include_adult=false&with_genres=35"
+    //try the request
+    try {
+        const response = await MovieDataService.get(url, options);
+        const movieData = response.data;
+        console.log("Responce:", movieData)
+        return movieData;
+    } 
+    catch (err) {
+        console.error(err);
+    }
+}
