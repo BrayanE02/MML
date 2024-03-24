2d7d01ef1e8e7273e5601261b14a69a1b70c2312
Oneshot369 <joshcpeck4@gmail.com>
2024-03-23 19:18:23 -0700

    fixed some css formating

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,22 +1,26 @@
 import "bootstrap"
 import "../css/Card.css"
-//the props paramiter is be the json for a movie
+import { useNavigate } from 'react-router-dom';
 
+//the props paramiter is be the json for a movie
 function Card(props) {
+  const navigate = useNavigate();
+
+    const handleClick = () => {
+      console.log("here")
+        navigate('/OneMoviePage')
+    };
   var ourMovie = props.movie;
   return (
-    <a href="./OneMoviePage">
-    <div className="card" style={{ width: "15rem", height: "10rem"}}>
-      <img src={"https://image.tmdb.org/t/p/w300/" + ourMovie.poster_path} className="card-img-top" alt={ourMovie.original_title + " poster"} />
-      <div className="card-body">
-        <h5 className="card-title">{ourMovie.original_title}</h5>
-        <p className="card-text">
-          {ourMovie.release_date}
-        </p>
+      <div className="card" onClick={handleClick} style={{ width: "15rem", height: "10rem", cursor: "pointer" }}>
+          <img src={"https://image.tmdb.org/t/p/w300/" + ourMovie.poster_path} className="card-img-top" alt={ourMovie.original_title + " poster"} />
+          <div className="card-body">
+            <h5 className="card-title">{ourMovie.original_title}</h5>
+            <p className="card-text">
+              {ourMovie.release_date}
+            </p>
+          </div> 
       </div>
-    </div>
-
-    </a>
   );
 }
 
