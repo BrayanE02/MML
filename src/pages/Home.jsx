51417e01909e1a518cc39418a257b4884598fa8f
Oneshot369 <joshcpeck4@gmail.com>
2024-04-01 12:56:03 -0700

    added hero autoplay

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import 'swiper/css'
 import "../css/Home.css"
 import SwiperBar from '../components/SwiperBar';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { EffectCards } from 'swiper/modules';
+import { EffectCards, Autoplay } from 'swiper/modules';
 
 function Home(params) {
     var moviesListComedies = params.topComedy;
@@ -19,9 +19,15 @@ function Home(params) {
             <Search updateSearch={params.changeSearch}></Search>
             <div className='carouselDiv'>
                 <Swiper
-                    effect={'cards'}
+                    effect='cards'
+                    autoplay={{
+                        delay:5000,
+                        disableOnInteraction: false,
+                        pauseOnMouseEnter: true,
+                    }}
+                    
                     grabCursor={true}
-                    modules={[EffectCards]}
+                    modules={[EffectCards, Autoplay]}
                     className="swiper-car"
                 >
                     <SwiperSlide className='swiper-slide-car' ><div className='img-div-slider'><img src={"https://image.tmdb.org/t/p/w500/" + backDrops[0]} className="d-block " alt="..." /><p className='img-text'>{backDrops[1]}</p></div></SwiperSlide>
